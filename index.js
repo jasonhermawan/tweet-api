24a87adf90e16d3ebc64f2c60b9c796eff73e3fd
jasonhermawan <jasonhermawans@gmail.com>
2023-11-01 11:25:12 +0700

    initial setup

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 2000;
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).send("<h1>API RUNNING</h1>")
+});
+
+// Define Router
+
+app.listen(PORT, () => {
+  console.log(`API RUNNING ON PORT ${PORT}`);
+});
